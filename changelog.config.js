f0023976b2e8a764dca4b9eae3b7b0ba72a69f46
codesche <codesche@gmail.com>
2024-09-13 16:33:59 +0900

    docs(BE): ✏️ backend commit
    테스트와 관련된 커밋 테스트를 진행하였습니다.

--- changelog.config.js
+++ changelog.config.js
@@ -5,7 +5,7 @@ module.exports = {
   maxMessageLength: 64,
   minMessageLength: 3,
   questions: ['type', 'scope', 'subject', 'body', 'breaking', 'issues', 'lerna'],
-  scopes: [],
+  scopes: ['FE', 'BE', "DO"],
   types: {
     deploy: {
       description: '배포',
